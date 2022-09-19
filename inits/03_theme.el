8abc961317abe4ef0e7fbd28217306c46bd3f987
Susumu Sasaki <susumu.sasaki.1967@gmail.com>
2022-09-19 18:30:11 +0900

    theme変更

--- inits/03_theme.el
+++ inits/03_theme.el
@@ -7,14 +7,18 @@
 (when window-system                       ; GUI時
 ;;  (load-theme 'deeper-blue t)
 ;;  (load-theme 'my-deeper-blue t)
-  (load-theme 'my-manoj-dark t)
+;;  (load-theme 'my-manoj-dark t)
+;;  (load-theme 'dark-blue t)
+  (load-theme 'deep-blue t)
   
   ;; 現在行に色をつける
   (global-hl-line-mode t)		  ; 現在行に色をつける
   )
 
 (unless window-system                     ; CUI時
-  (load-theme 'manoj-dark t)
+;;  (load-theme 'manoj-dark t)
+;;  (load-theme 'dark-blue t)
+  (load-theme 'deep-blue t)
 
   ;; 現在行を下線
   (setq hl-line-face 'underline)	  ; 現在行をアンダーライン
